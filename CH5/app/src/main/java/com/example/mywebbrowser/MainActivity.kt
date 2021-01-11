9ecc35e56c1faf4159be26621ab73c7a64491202
ColorLion <godownfight@naver.com>
2021-01-11 17:41:04 +0900

    Webview

--- CH5/app/src/main/java/com/example/mywebbrowser/MainActivity.kt
+++ CH5/app/src/main/java/com/example/mywebbrowser/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.mywebbrowser
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.EditorInfo
+import android.webkit.WebViewClient
+import kotlinx.android.extensions.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // web view 기본 설정
+        webView.apply{
+            settings.javaScriptEnabled = true
+            webViewClient = WebViewClient()
+        }
+
+        webView.loadUrl("http://www.piugame.com/piu.xx/")
+        
+        //keyboard 정의
+        var searchUrl: String? = null
+        var checkHttpString = IntRange(0, 3)
+        urlEditText.setOnEditorActionListener{_, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_SEARCH){
+                if (urlEditText.text.toString().slice(checkHttpString) == "http"){
+                    webView.loadUrl(urlEditText.text.toString())
+                } else {
+                    searchUrl = "https://" + urlEditText.text.toString()
+                    webView.loadUrl(searchUrl)
+                }
+
+                true
+            }else{
+                false
+            }
+        }
     }
 }
