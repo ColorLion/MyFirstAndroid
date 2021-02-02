e263e229282c8bcb9cdc88d4105ef7dc0fde88ea
ColorLion <godownfight@naver.com>
2021-02-02 17:36:37 +0900

    addon: Pylophone

--- CH10/app/src/main/java/com/example/pylophone/MainActivity.kt
+++ CH10/app/src/main/java/com/example/pylophone/MainActivity.kt
@@ -5,7 +5,9 @@ import android.media.SoundPool
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private val soundPool = if(Build.VERSION.SDK_INT  >= Build.VERSION_CODES.LOLLIPOP){
@@ -13,6 +15,9 @@ class MainActivity : AppCompatActivity() {
     }else{
         SoundPool(12, AudioManager.STREAM_MUSIC, 0)
     }
+    private var left = 1.0f
+    private var right = 1.0f
+    private var rate = 1.0f
 
     private val sounds = listOf(
         Pair(R.id.note1, R.raw.bb),        Pair(R.id.note2, R.raw.gachi),        Pair(R.id.note3, R.raw.hia),
@@ -26,14 +31,52 @@ class MainActivity : AppCompatActivity() {
 
     private fun tune(pitch: Pair<Int, Int>){
         val soundId = soundPool.load(this, pitch.second, 1)
+
         findViewById<TextView>(pitch.first).setOnClickListener{
-            soundPool.play(soundId, 1.0f, 1.0f, 0, 0, 1.0f)
+            soundPool.play(soundId, left, right, 0, 0, rate)
         }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        leftVol.setOnCheckedChangeListener{ buttonView, isChecked ->
+            if(isChecked){
+                right = 0.3f
+                leftUpText.setText("LEFT UP")
+            } else {
+                right = 1.0f
+                leftUpText.setText("OFF")
+            }
+        }
+        rightVol.setOnCheckedChangeListener{ buttonView, isChecked ->
+            if(isChecked){
+                left = 0.3f
+                rightUpText.setText("RIGHT UP")
+            } else {
+                left = 1.0f
+                rightUpText.setText("OFF")
+            }
+        }
+        slowRateSwitch.setOnCheckedChangeListener{ buttonView, isChecked ->
+            if(isChecked){
+                rate = 0.8f
+                slowRateText.setText("RATE x0.5")
+            } else {
+                rate = 1.0f
+                rightUpText.setText("OFF")
+            }
+        }
+        fastRateSwitch.setOnCheckedChangeListener{ buttonView, isChecked ->
+            if(isChecked){
+                rate = 1.2f
+                fastRateText.setText("RATE x2")
+            } else {
+                rate = 1.0f
+                fastRateText .setText("OFF")
+            }
+        }
 
         sounds.forEach{ tune(it)}
     }
